5b3919b85479c3377598c24260d444a0e8356cb8
Nery David Méndez Méndez <nerydavidmendezmendez@outlook.com>
2024-03-21 16:25:57 -0600

    Se añadio la vista previa de imagen en usuarios, detalles y borrar

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace KDSB_JMRH_NNNN_NDMM.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
