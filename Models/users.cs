731f6ea17f58226e26bb7c52d7908499e0a41b19
Natasha Nicole Navarro Nolasco <nerydavidmendezmendez@outlook.com>
2024-03-21 23:18:56 -0600

    Anotaciones de datos del modelo users

--- Models/users.cs
+++ Models/users.cs
@@ -7,15 +7,16 @@ namespace KDSB_JMRH_NNNN_NDMM.Models
     {
             public int Id { get; set; }
         [Display(Name = "Nombre de usuario:")]
-       
+        [Required(ErrorMessage = "El Nombre de Usuario es obligatorio")]
         public string UserName { get; set; }
         [Display(Name = "Contraseña:")]
+        [Required(ErrorMessage = "La Contraseña es obligatoria")]
         public string Password { get; set; }
         [Display(Name = "Correo electronico:")]
-        
+        [Required(ErrorMessage = "El Correo electronico es obligatorio")]
         public string Email { get; set; }
         [Display(Name = "Estado:")]
-        
+        [Required(ErrorMessage = "Digite su estado")]
         public int Status { get; set; }
         [Display(Name = "Imagen:")]
         public byte[] Image { get; set; }
